9fad5ff589df3f8b819f860d2b80f49917fd4642
slovomir <webkey@mail.ru>
2022-09-16 00:31:42 +0300

    json file

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask_limiter.util import get_remote_address
 
 import requests
 from bs4 import BeautifulSoup
-
-from datetime import datetime
 import pytz
+
+from pathlib import Path
+from datetime import datetime
+import json
 
 import logging
 
@@ -84,39 +86,81 @@ class Converter:
                     self.euro_bnb = float(item[2])
 
     def get_qiwi(self):
-        if self.byn:
+        if not self.qiwi_byn:
             self.qiwi_byn = 100 / 0.85 / 0.95 / self.byn
-            self.last_update = datetime.now(pytz.timezone('Europe/Minsk'))
-        else:
-            return False
 
     def do(self):
         dt_now = datetime.now(pytz.timezone('Europe/Minsk'))
+
         if self.last_update:
             if dt_now.date() > self.last_update.date():
-                self.byn = None
-                self.qiwi_byn = None
-                self.dollar_mt = None
-                self.dollar_bnb = None
-                self.euro_mt = None
-                self.euro_bnb = None
-                self.last_update = None
+                self.clear_data()
+                self.last_update = dt_now
+            return self.make_response()
+        else:
+            if Path('json/rate.json').is_file():
+                with open('json/rate.json', 'r') as f:
+                    data = json.load(f)
+                    if data:
+                        data['last_update'] = datetime.strptime(data['last_update'], '%d/%m/%Y, %H:%M:%S')
+                        if dt_now.date() == data['last_update'].date():
+                            self.clear_data()
+                            self.last_update = dt_now
+                            return self.make_response()
 
+                        self.byn = data['byn']
+                        self.qiwi_byn = data['qiwi_byn']
+                        self.dollar_mt = data['dollar_mt']
+                        self.dollar_bnb = data['dollar_bnb']
+                        self.euro_mt = data['euro_mt']
+                        self.euro_bnb = data['euro_bnb']
+                        self.last_update = data['last_update'].strptime()
+
+                        response = {'message': 'OK',
+                                    'byn': data['byn'],
+                                    'qiwi_byn': data['qiwi_byn'],
+                                    'dollar_mt': data['dollar_mt'], 'dollar_bnb': data['dollar_bnb'],
+                                    'euro_mt': data['euro_mt'], 'euro_bnb': data['euro_bnb'],
+                                    'last_update': f"Курсы обновлены {data['last_update'].strftime('%d.%m.%Y')} в {data['last_update'].strftime('%H:%M')} (Минск)",
+                                    'code': 200}
+                        return response
+                    else:
+                        return self.make_response()
+            else:
+                self.last_update = dt_now
+                return self.make_response()
+
+    def make_response(self):
         self.get_byn()
         self.get_currency()
 
         self.get_qiwi()
 
         if all(self.__dict__.values()):
-            return {'message': "OK",
-                    'byn': self.byn,
-                    'qiwi_byn': self.qiwi_byn,
-                    'dollar_mt': self.dollar_mt, 'dollar_bnb': self.dollar_bnb,
-                    'euro_mt': self.euro_mt, 'euro_bnb': self.euro_bnb,
-                    'last_update': f"Курсы обновлены {self.last_update.strftime('%d.%m.%Y')} в {self.last_update.strftime('%H:%M')} (Минск)",
-                    'code': 200}
+            response = {'byn': self.byn,
+                        'qiwi_byn': self.qiwi_byn,
+                        'dollar_mt': self.dollar_mt, 'dollar_bnb': self.dollar_bnb,
+                        'euro_mt': self.euro_mt, 'euro_bnb': self.euro_bnb,
+                        'last_update': self.last_update.strftime("%d/%m/%Y, %H:%M:%S")}
+
+            with open('json/rate.json', 'w', encoding='utf-8') as f:
+                json.dump(response, f, ensure_ascii=False)
+
+            response['last_update'] = f"Курсы обновлены {self.last_update.strftime('%d.%m.%Y')} в {self.last_update.strftime('%H:%M')} (Минск)"
+            response['message'] = 'OK'
+            response['code'] = 200
+            return response
         else:
             return {'message': 'Bad request', 'code': 500}
+
+    def clear_data(self):
+        self.byn = None
+        self.qiwi_byn = None
+        self.dollar_mt = None
+        self.dollar_bnb = None
+        self.euro_mt = None
+        self.euro_bnb = None
+        self.last_update = None
 
 
 converter = Converter()
@@ -132,13 +176,16 @@ def qiwi_post():
     data = request.data.decode('utf-8')
     logger.info(f"Request: {data}")
     result = converter.do()
-    response = make_response(jsonify(message=result['message'],
-                                     byn=result['byn'],
-                                     qiwi_byn=result['qiwi_byn'],
-                                     dollar_mt=result['dollar_mt'], dollar_bnb=result['dollar_bnb'],
-                                     euro_mt=result['euro_mt'], euro_bnb=result['euro_bnb'],
-                                     last_update=result['last_update'],
-                                     status=result['code']))
+    if result['code'] == 200:
+        response = make_response(jsonify(message=result['message'],
+                                         byn=result['byn'],
+                                         qiwi_byn=result['qiwi_byn'],
+                                         dollar_mt=result['dollar_mt'], dollar_bnb=result['dollar_bnb'],
+                                         euro_mt=result['euro_mt'], euro_bnb=result['euro_bnb'],
+                                         last_update=result['last_update'],
+                                         status=result['code']))
+    else:
+        response = make_response(jsonify(message=result['message'], status=result['code']))
     return response
 
 
